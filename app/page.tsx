71409e461585561c157d8ed1d6cbcf947388263e
Chase Christensen <chaserchristensen@gmail.com>
2024-03-28 17:41:15 -0600

    🚀 (layout.tsx): update font style import and class names in layout component   ✨ (page.tsx): add state management for LED strip properties and generate colors   ♻️ (package-lock.json): upgrade geist package to version 1.3.0 with peer dependency support   ⬆️ (package.json): update dependencies and add turbo mode to the dev script

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,198 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [columnCount, setColumnCount] = useState(24); // Default to 24 columns
+  const [lightCount, setLightCount] = useState(250); // Default to 250 lights
+  const [lightStripLength, setLightStripLength] = useState(50); // Default length of each light strip
+  const [lightStripsAmount, setLightStripsAmount] = useState(5); // Default amount of light strips
+  const [isSerpentine, setIsSerpentine] = useState("serpentine"); // Use string value for initial state
+  // Calculate lightCount based on lightStripLength and lightStripsAmount
+  useEffect(() => {
+    setLightCount(lightStripLength * lightStripsAmount);
+  }, [lightStripLength, lightStripsAmount]);
+
+  const generateColor = (index: number, total: number) => {
+    const hue = (360 * index) / total;
+    return `hsl(${hue}, 100%, 75%)`;
+  };
+  const lightStripColors = Array.from(
+    { length: lightStripsAmount },
+    (_, index) => generateColor(index, lightStripsAmount)
+  );
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col justify-start p-10">
+      <div className="flex flex-row justify-between items-baseline">
+        <h1 className="text-xl mb-3">LED Strip to Matrix Tool</h1>
+        <p>by Chase Cee</p>
+      </div>
+
+      <div className="flex flex-row gap-5 self-start text-sm mb-5">
+        <div className="flex flex-col gap-1">
+          <label
+            className="opacity-80 font-semibold"
+            htmlFor="light_strip_length"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Light Strip Length
+          </label>
+          <input
+            type="number"
+            value={lightStripLength}
+            id="light_strip_length"
+            onChange={(e) => setLightStripLength(Number(e.target.value))}
+            className=" text-white bg-transparent p-2 rounded ring-[1.5px] ring-white/50 max-w-[150px]"
+          />
+        </div>
+        <div className="flex flex-row align-baseline">
+          <span>x</span>
+        </div>
+        <div className="flex flex-col gap-1">
+          <label
+            className="opacity-80 font-semibold"
+            htmlFor="light_strips_amount"
+          >
+            Number of Strips
+          </label>
+          <input
+            type="number"
+            value={lightStripsAmount}
+            id="light_strips_amount"
+            onChange={(e) => setLightStripsAmount(Number(e.target.value))}
+            className=" text-white bg-transparent p-2 rounded ring-[1.5px] ring-white/50 max-w-[150px]"
+          />
+        </div>
+        <div className="flex flex-row align-baseline">
+          <span>=</span>
+        </div>
+        <div className="flex flex-col gap-1">
+          <label className="opacity-80 font-semibold" htmlFor="lights_count">
+            Pixel Total
+          </label>
+          <input
+            type="number"
+            value={lightCount}
+            id="lights_count"
+            disabled // Make the field disabled
+            className=" text-white bg-transparent p-2 rounded ring-[1.5px] ring-white/50 max-w-[150px] opacity-50 cursor-not-allowed"
+          />
+        </div>
+        <div className="flex flex-row gap-5">
+          <div className="flex flex-col gap-1">
+            <label className="opacity-80 font-semibold" htmlFor="cols_wide">
+              Matrix Columns
+            </label>
+            <input
+              type="number"
+              value={columnCount}
+              id="cols_wide"
+              onChange={(e) => setColumnCount(Number(e.target.value))}
+              className=" text-white bg-transparent p-2 rounded ring-[1.5px] ring-white/50 max-w-[150px]"
             />
-          </a>
+          </div>
+
+          <div className="flex flex-col ">
+            <label className="opacity-80 font-semibold">Layout</label>
+            <div className="flex flex-col justify-center">
+              <label className="flex flex-row gap-1 items-center">
+                <input
+                  type="radio"
+                  name="layout"
+                  value="serpentine"
+                  checked={isSerpentine === "serpentine"} // Ensure this comparison is against a string
+                  onChange={() => setIsSerpentine("serpentine")}
+                  className="mb-0 "
+                />
+                <span>Serpentine</span>
+              </label>
+              <label className="flex flex-row gap-1 items-center">
+                <input
+                  type="radio"
+                  name="layout"
+                  value="parallel"
+                  checked={isSerpentine === "parallel"} // Ensure this comparison is against a string
+                  onChange={() => setIsSerpentine("parallel")}
+                  className="mb-0"
+                />
+                <span>Parallel</span>
+              </label>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="relative w-full h-full">
+        <div
+          className="grid gap-[1px]"
+          style={{
+            gridTemplateColumns: `repeat(${columnCount}, minmax(0, 1fr))`,
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {Array.from({ length: lightCount }, (_, index) => {
+            let adjustedIndex = index;
+            const row = Math.floor(index / columnCount);
+            const isRowEven = row % 2 === 0;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            switch (isSerpentine) {
+              case "serpentine":
+                if (!isRowEven) {
+                  const startOfRow = row * columnCount;
+                  const endOfRow = startOfRow + columnCount - 1;
+                  adjustedIndex = endOfRow - (index - startOfRow);
+                }
+                break;
+              case "parallel":
+                // In parallel mode, the adjustedIndex remains the same as the original index
+                break;
+              case "wrap":
+                if (row % lightStripsAmount === 0 && row !== 0) {
+                  // When a new set of strips starts, wrap back to the start of the matrix
+                  adjustedIndex = index % (columnCount * lightStripsAmount);
+                }
+                break;
+              default:
+                // Handle any other unexpected cases
+                break;
+            }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            const stripIndex =
+              Math.floor(adjustedIndex / lightStripLength) % lightStripsAmount;
+            const bgColor = lightStripColors[stripIndex];
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+            return (
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center aspect-square rounded-sm"
+                style={{ backgroundColor: bgColor }}
+              >
+                <span className="text-opacity-50 text-[10px] text-black">
+                  {adjustedIndex + 1}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <div
+          className="grid relative z-10 hidden"
+          style={{
+            gridTemplateColumns: `repeat(${columnCount}, minmax(0, 1fr))`,
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {Array.from({ length: 40 }, (_, index) => {
+            index += 1; // Offset index to start at 1
+            const bgColor = "bg-yellow-100/0";
+
+            return (
+              <div
+                key={index}
+                className={`flex flex-col items-center justify-center border aspect-[3/2] border-red-500 ${bgColor}`}
+                style={{ gridColumn: "span 3", gridRow: "span 2" }}
+              >
+                <span className="text-opacity-100 text-[16px] text-red-500">
+                  {index}
+                </span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </main>
   );
